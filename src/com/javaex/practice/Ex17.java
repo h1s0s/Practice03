c04a4dcb397ca27e83b2909208e27e6ee3d229c2
한상선 <hs94122@naver.com>
2021-11-17 22:04:27 +0900

    2021-11-17 22:04

--- src/com/javaex/practice/Ex17.java
+++ src/com/javaex/practice/Ex17.java
@@ -1,10 +1,25 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex17 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요: ");
+		int num = sc.nextInt();
+		
+		for(int y=num; y>0; y--,num--) {
+			System.out.print("*");
+			for(int x=num-1;x>0; x-- ) {
+				System.out.print("*");
+			}System.out.println("");
+			
+		}
+		
+		sc.close();
 	}
 
 }
