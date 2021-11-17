64de1b97494f5422c807bf7d217a2939ec9c0a8a
한상선 <hs94122@naver.com>
2021-11-17 21:36:14 +0900

    2021-11-17 21:36

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -3,11 +3,12 @@ package com.javaex.practice;
 public class Ex08 {
 
 	public static void main(String[] args) {
-		//막힌문제
-		int dan = 2;
 		
-		for(int i=1; i<10; i++) {
-			System.out.print(dan + "*" + i + "=" + dan*i + " ");
+		int y=2;
+		for(int x=1; x<10; x++) {
+			for(y=2; y<10; y++) {
+				System.out.print(y+"*"+x+"="+(y*x)+"\t");
+			}System.out.println("");
 		}
 
 	}
