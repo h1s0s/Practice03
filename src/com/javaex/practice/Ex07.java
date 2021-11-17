4d166c2e23c0f1a3f5278485e7383148d1fb3022
한상선 <hs94122@naver.com>
2021-11-17 21:27:24 +0900

    2021-11-17 21:27

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Ex07 {
 
 	public static void main(String[] args) {
-		//막힌 문제
+		//다시 복습하기!!
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 입력하세요.");
 		int num = sc.nextInt();
-		int num2 = 1;
-		for(int y=0; y<=num; y++) {
-			for(int x=0; x<=y; x++) {
-				System.out.print(num2);
+		
+		for(int y=1; y<=num; y++) {
+			for(int x=0; x<y; x++) {
+				System.out.print(y);
 			}
 			System.out.println("");
 		}
