0bbc1160f424a79d811b347ee5b9baf41e6366b1
한상선 <hs94122@naver.com>
2021-11-18 11:09:00 +0900

    2021-11-18 11:08

--- src/com/javaex/practice/Ex12.java
+++ src/com/javaex/practice/Ex12.java
@@ -5,23 +5,17 @@ import java.util.Scanner;
 public class Ex12 {
 
 	public static void main(String[] args) {
-		// 너무 어려움.. 풀다 막힘
+
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("숫자를 입력하세요.");
+		System.out.println("숫자를 입력하세요");
 		int num = sc.nextInt();
-		for(int i=1, e=0; i<=num; i++) {
-			if(num%2==0) {//입력한 숫자가 짝수일때.
-				e= e+i;
-			} else if(num<=0) {//입력한 숫자가 음수거나 0일때.
-				System.out.println("음수와 0은 적용할 수 없습니다.");
-			} else {//입력한 숫자가 홀수일때.
-				
-			}
-				e = i;
-			System.out.println("결과값"+e);
+		int num2 = 1;
+		for(int i=1; i<=num; i++) {
+			num2 = num2*i;
 		}
 		
+		System.out.println("\n"+"합계:"+num2);
 		
 		sc.close();
 	}
