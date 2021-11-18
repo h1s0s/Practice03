cd8dba9d537d71ffa1d506dd548e512c508d357c
한상선 <hs94122@naver.com>
2021-11-18 10:18:57 +0900

    2021-11-18 10:18

--- src/com/javaex/practice/Ex10.java
+++ src/com/javaex/practice/Ex10.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Ex10 {
 
 	public static void main(String[] args) {
-		//막힌문제.. 이걸 반복문으로??
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("숫자를 입력하세요.");
 		int num = 0;
-		for(int i=1; i<6; i++) {
+		for(int i=1; i<6; i++) {//5번 반복하고 끝나게
 			System.out.print("숫자: ");
 			int push = sc.nextInt();
-			if (push<num) {//입력한 숫자가 크면
+			if (push<num) {//입력한 숫자가 작으면
 				//아무것도 없이 종료.
-			} else if(push>=num) {//입력한 숫자가 작으면
+			} else if(push>=num) {//입력한 숫자가 크면 num에 대입
 				num = push;
 				
 			}
